9d93bea59ffd2b75d45dff9d5ce2974f84cce8e2
danielsilvayanez <daniel.silva@xing.com>
2020-07-06 21:24:34 +0200

    Update TaskListItem button and story

--- src/components/TaskListItem.js
+++ src/components/TaskListItem.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import tasks from './tasks.json'
+import TaskButton from './TaskButton'
 
 export default function TaskListItem() {
   return (
     <StyledSection>
       {tasks.map((task) => (
-        <button key={task.call_number}>{task.call_number}</button>
+        <TaskButton key={task.call_number} callNo={task.call_number} />
       ))}
     </StyledSection>
   )
@@ -18,20 +19,4 @@ const StyledSection = styled.section`
   align-items: center;
   flex-direction: column;
   height: 100vh;
-
-  button {
-    margin-top: 10px;
-    color: hotpink;
-    width: 90%;
-    height: 80px;
-    border-radius: 9px;
-    background-color: #464646;
-    font-size: 36px;
-    font-weight: bold;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: normal;
-    letter-spacing: normal;
-    color: #ffffff;
-  }
 `
