dc2f502f32393d12cc7220ee61662068293d81e4
danielsilvayanez <daniel.silva@xing.com>
2020-07-06 21:25:07 +0200

    Update Header position and story

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import Logo from '../icons/C&TSLogo.jpg'
+import ctsLogo from '../icons/ctsLogo.svg'
 
 let currentDate = new Date()
 const day = String(currentDate.getDate()).padStart(2, '0')
@@ -14,26 +14,27 @@ export default function Header() {
     <StyledHeader>
       <h1>{currentDate}</h1>
       <h1>Hallo, Fahrer</h1>
-      <img src={Logo} alt="website logo" />
+      <img src={ctsLogo} alt="website logo" />
     </StyledHeader>
   )
 }
 
 const StyledHeader = styled.header`
-  border: solid 1px #464646;
-  background-color: #464646;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 48px;
+  background-color: var(--primary-grey-mid);
   padding: 10px;
   height: 100px;
+  box-shadow: 0 2px 5px 3px grey;
 
   img {
     position: fixed;
-    top: 0;
-    right: 2px;
+    top: -8px;
+    right: -6px;
     height: 64px;
     border-bottom-left-radius: 9px;
     border-bottom-right-radius: 9px;
-    box-shadow: -1px 5px 8px 0 rgba(0, 0, 0, 0.5);
-    border: solid 1px #464646;
-    background-color: #464646;
   }
 `
---
+++ src/components/Header.stories.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Header'
+import GlobalStyles from './GlobalStyles'
+
+export default {
+  title: 'Header',
+  component: Header,
+}
+
+export const ToStorybook = () => (
+  <>
+    <GlobalStyles />
+    <Header />
+  </>
+)
+
+ToStorybook.story = {
+  name: 'Header',
+}
