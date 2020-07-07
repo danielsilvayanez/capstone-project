9b7bc02dc5eebbf6dbbfbcf8180ea8cc7d18b661
danielsilvayanez <daniel.silva@xing.com>
2020-07-07 15:45:48 +0200

    Add ul to task item

--- src/components/TaskListItem.js
+++ src/components/TaskListItem.js
@@ -6,17 +6,31 @@ import TaskButton from './TaskButton'
 export default function TaskListItem() {
   return (
     <StyledSection>
-      {tasks.map((task) => (
-        <TaskButton key={task.call_number} callNumber={task.call_number} />
-      ))}
+      <ul>
+        {tasks.map((task) => (
+          <li>
+            <TaskButton key={task.call_number} callNumber={task.call_number} />
+          </li>
+        ))}
+      </ul>
     </StyledSection>
   )
 }
 
 const StyledSection = styled.section`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
   height: 100vh;
+
+  ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding-left: 0;
+    width: 100%;
+
+    li {
+      width: 90%;
+      list-style-type: none;
+    }
+  }
 `
