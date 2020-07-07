16c36834644b8a114f2dabf94f72e321b8372cdd
danielsilvayanez <daniel.silva@xing.com>
2020-07-07 15:46:09 +0200

    Change due to ul logic

--- src/components/TaskButton.js
+++ src/components/TaskButton.js
@@ -7,19 +7,15 @@ export default function TaskButton({ callNumber, onClick }) {
 
 const StyledButton = styled.button`
   margin-top: 10px;
-  width: 90%;
-  height: 80px;
   border-top-left-radius: 9px;
   border-top-right-radius: 9px;
   background-color: var(--primary-white);
   font-size: 36px;
   font-weight: bold;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
   letter-spacing: normal;
   color: var(--primary-grey-mid);
   box-shadow: 3px 4px 5px 0px rgba(0, 0, 0, 0.38);
+  width: 100%;
 
   :hover {
     background-color: var(--primary-grey-mid);
