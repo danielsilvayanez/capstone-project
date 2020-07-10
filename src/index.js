a6212ebdd433525d87dc111436da5bb54ea46a0d
danielsilvayanez <daniel.silva@xing.com>
2020-07-10 18:47:12 +0200

    Update routes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import GlobalStyles from './components/GlobalStyles'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <Router>
+      <GlobalStyles />
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
