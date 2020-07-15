c8546ce3be1865d8ede1a025e93799d6735e8d3d
danielsilvayanez <daniel.silva@xing.com>
2020-07-15 17:32:30 +0200

    Update test with click and timestamp

--- src/components/TaskListItem.test.js
+++ src/components/TaskListItem.test.js
@@ -1,18 +1,23 @@
+//taskListItem.test.js
 import React from 'react'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen, getAllByTestId } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import TaskListItem from './TaskListItem'
+import userEvent from '@testing-library/user-event'
 import dayjs from 'dayjs'
-
-let timeStamp = dayjs().format('HH:mm')
 
 beforeEach(() => {
   render(<TaskListItem />)
 })
 
 describe('TaskListItem.js test ', () => {
+  let timeStamp = dayjs().format('HH:mm')
   it('Should initialize 3 buttons and 3 divs', () => {
     expect(screen.getAllByTestId('button-test').length).toBe(3)
     expect(screen.getAllByTestId('div-test').length).toBe(3)
+    expect(screen.getByText('Start' && 'Ankunft' && 'Erledigt'))
+    userEvent.click(screen.getByText('Start'))
+    expect(screen.getAllByTestId('div-test')[0].textContent).toBe(timeStamp)
+    screen.debug(screen.getAllByTestId('div-test')[0].innerText)
   })
 })
