049c168217bbcd5521ca379ec46e333a512f905e
danielsilvayanez <daniel.silva@xing.com>
2020-07-15 17:29:55 +0200

    Update button with click test

--- src/components/TaskButton.test.js
+++ src/components/TaskButton.test.js
@@ -3,22 +3,19 @@ import TaskButton from './TaskButton'
 import { render, fireEvent, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
-beforeEach(() => {
-  render(<TaskButton callNumber={'Call-Number'} />)
-})
-
-//@to-do - onClick={mockCallBack}
-
-describe('TaskButton.js test', () => {
-  it('displays Button with task number', () => {
+describe('TaskButton', () => {
+  const mockCallback = jest.fn()
+  beforeEach(() => {
+    render(<TaskButton callNumber={'Call-Number'} onClick={mockCallback} />)
+  })
+  it('shows a task number', () => {
     expect(screen.getByRole('button'))
   })
-  it('Button should render a text', () => {
+  it('renders a text', () => {
     expect(screen.getByText('Call-Number')).toBeInTheDocument()
   })
-  /*it('Button should be clickable', (mockCallBack) => {
-    const mockCallBack = jest.fn()
+  it('works with callback', () => {
     fireEvent.click(screen.getByRole('button'))
-    expect(mockCallBack).toHaveBeenCalledTimes(1)
-  })*/
+    expect(mockCallback).toHaveBeenCalledTimes(1)
+  })
 })
