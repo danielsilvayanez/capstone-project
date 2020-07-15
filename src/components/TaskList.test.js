6410d7a048c296fa2a81c3da004861e3d1693cc5
danielsilvayanez <daniel.silva@xing.com>
2020-07-15 17:31:16 +0200

    Update task list test with clicks and routing

--- src/components/TaskList.test.js
+++ src/components/TaskList.test.js
@@ -1,17 +1,31 @@
 import '@testing-library/jest-dom/extend-expect'
-import React from 'react'
 import { render, screen } from '@testing-library/react'
-import TaskList from './TaskList'
+import React from 'react'
 import tasks_mock from '../components/__mocks__/tasks_mock.json'
+import TaskList from './TaskList'
+import userEvent from '@testing-library/user-event'
 
-beforeEach(() => {
-  render(<TaskList tasks={tasks_mock} />)
-})
+const mockHistoryPush = jest.fn()
+jest.mock('react-router-dom', () => ({
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}))
 
-describe('TaskList.js test', () => {
-  it('should render expected length of 5 Call-Numbers', () => {
-    expect(screen.getAllByTestId('call_number-test').length).toBe(
+describe('TaskList', () => {
+  beforeEach(() => {
+    render(<TaskList tasks={tasks_mock} />)
+  })
+  it('should render 5 Call-Numbers', () => {
+    expect(screen.getAllByText('Call-Number', { exact: false })).toHaveLength(
       tasks_mock.length
+    )
+  })
+  it('should create the right Link for each TaskButton', () => {
+    userEvent.click(screen.getByText('Call-Number1'))
+    expect(mockHistoryPush).toHaveBeenCalledTimes(1)
+    expect(mockHistoryPush).toHaveBeenCalledWith(
+      '/taskpagedetails/Call-Number1'
     )
   })
 })
