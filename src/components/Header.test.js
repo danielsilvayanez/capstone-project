a72eb448e5d862305d9b22952f7c7dccb7e3e2f9
danielsilvayanez <daniel.silva@xing.com>
2020-07-15 11:41:03 +0200

    Add beforeEch to Header

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import Header from './Header'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 import dayjs from 'dayjs'
 
 const currentDate = dayjs().format('DD.MM.YYYY')
 
-describe('Header test', () => {
+beforeEach(() => {
+  render(<Header />)
+})
+
+describe('Header.js test', () => {
   it('H1 displays current date', () => {
-    const { queryByText } = render(<Header />)
-    expect(queryByText(currentDate).tagName).toBe('H1')
+    expect(screen.queryByText(currentDate).tagName).toBe('H1')
   })
 
   it('H2 displays test and drivers name', () => {
-    const { queryByText } = render(<Header />)
-    expect(queryByText(currentDate).tagName).toBe('H1')
-    expect(queryByText('Hallo,').tagName).toBe('H2')
+    expect(screen.queryByText('Hallo,').tagName).toBe('H2')
   })
 })
