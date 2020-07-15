a52650d6ffb1745704fa096f29173fbe4a01b9a4
danielsilvayanez <daniel.silva@xing.com>
2020-07-15 11:40:02 +0200

    Import json file to app.js

--- src/App.js
+++ src/App.js
@@ -4,24 +4,40 @@ import Dashboard from './pages/Dashboard'
 import TaskPageDetails from './/pages/TaskPageDetails'
 import NotFound from './pages/NotFound'
 import { Route, Switch } from 'react-router-dom'
+import styled from 'styled-components'
+import tasks from './components/tasks.json'
 
 function App() {
   const driver = 'Will Smith'
 
   return (
-    <>
+    <AppGrid>
       <Header name={driver} />
-      <Switch>
-        <Route path="/dashboard" component={Dashboard} />
-        <Route
-          path="/taskpagedetails/:callNumber"
-          component={TaskPageDetails}
-        />
-        <Route exact path="/" component={Dashboard} />
-        <Route component={NotFound} />
-      </Switch>
-    </>
+      <StyledMain>
+        <Switch>
+          <Route
+            path="/dashboard"
+            component={() => <Dashboard tasks={tasks} />}
+          />
+          <Route
+            path="/taskpagedetails/:callNumber"
+            component={TaskPageDetails}
+          />
+          <Route exact path="/" component={() => <Dashboard tasks={tasks} />} />
+          <Route component={NotFound} />
+        </Switch>
+      </StyledMain>
+    </AppGrid>
   )
 }
 
 export default App
+
+const AppGrid = styled.div`
+  display: grid;
+  grid-template-rows: 100px auto;
+  height: 100vh;
+`
+const StyledMain = styled.main`
+  overflow: scroll;
+`
