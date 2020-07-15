708ccd770c77f465beb7e4862aa5d82fffd6f20f
danielsilvayanez <daniel.silva@xing.com>
2020-07-15 11:44:40 +0200

    Update Tasklist test with tasks_mock json

--- src/components/TaskList.test.js
+++ src/components/TaskList.test.js
@@ -2,18 +2,16 @@ import '@testing-library/jest-dom/extend-expect'
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import TaskList from './TaskList'
-import { NavLink } from 'react-router-dom'
+import tasks_mock from '../components/__mocks__/tasks_mock.json'
 
 beforeEach(() => {
-  render(
-    <NavLink>
-      <TaskList />
-    </NavLink>
-  )
+  render(<TaskList tasks={tasks_mock} />)
 })
 
 describe('TaskList.js test', () => {
-  it('should render expected length of Call-Numbers', () => {
-    expect(screen.getAllByTestId('call_number')).toBe(5)
+  it('should render expected length of 5 Call-Numbers', () => {
+    expect(screen.getAllByTestId('call_number-test').length).toBe(
+      tasks_mock.length
+    )
   })
 })
