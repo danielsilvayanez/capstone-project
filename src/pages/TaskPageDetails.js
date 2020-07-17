2878ee5dcc34da50059fb84bd7dae23744be1c7c
danielsilvayanez <daniel.silva@xing.com>
2020-07-17 12:44:27 +0200

    Update receiving props and finding each element

--- src/pages/TaskPageDetails.js
+++ src/pages/TaskPageDetails.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TaskListItem from '../components/TaskListItem'
 import TaskButton from '../components/TaskButton'
 import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
 
-export default function TaskPageDetails(tasks) {
+export default function TaskPageDetails({ tasks }) {
   const { callNumber } = useParams()
+  const [task, setTask] = useState(null)
+
+  useEffect(() => {
+    setTask(tasks.find((task) => task.call_number === callNumber))
+  }, [tasks])
 
   return (
     <StyledDiv>
-      <TaskButton callNumber={callNumber}>{callNumber}</TaskButton>
-      <TaskListItem tasks={tasks} />
+      {task && (
+        <>
+          <TaskButton callNumber={task.call_number}>
+            {task.call_number}
+          </TaskButton>
+          <TaskListItem task={task} setTask={setTask} />
+        </>
+      )}
     </StyledDiv>
   )
 }
