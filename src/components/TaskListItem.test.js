c4f37b059234e9263decf82fce1a8bce1b017131
danielsilvayanez <daniel.silva@xing.com>
2020-07-17 12:41:32 +0200

    Update test timestamp

--- src/components/TaskListItem.test.js
+++ src/components/TaskListItem.test.js
@@ -10,14 +10,22 @@ beforeEach(() => {
   render(<TaskListItem />)
 })
 
-describe('TaskListItem.js test ', () => {
+describe('TaskListItem.js test', () => {
   let timeStamp = dayjs().format('HH:mm')
   it('Should initialize 3 buttons and 3 divs', () => {
     expect(screen.getAllByTestId('button-test').length).toBe(3)
     expect(screen.getAllByTestId('div-test').length).toBe(3)
     expect(screen.getByText('Start' && 'Ankunft' && 'Erledigt'))
+  })
+  it('Should click on 3 buttons and check for timestamp', () => {
     userEvent.click(screen.getByText('Start'))
     expect(screen.getAllByTestId('div-test')[0].textContent).toBe(timeStamp)
-    screen.debug(screen.getAllByTestId('div-test')[0].innerText)
+    //screen.debug(screen.getAllByTestId('div-test')[0].innerText)
+    userEvent.click(screen.getByText('Ankunft'))
+    expect(screen.getAllByTestId('div-test')[1].textContent).toBe(timeStamp)
+    //screen.debug(screen.getAllByTestId('div-test')[1].innerText)
+    userEvent.click(screen.getByText('Erledigt'))
+    expect(screen.getAllByTestId('div-test')[2].textContent).toBe(timeStamp)
+    //screen.debug(screen.getAllByTestId('div-test')[2].innerText)
   })
 })
