e41a588d524a4ab6c1a6f1cf8a00bccf63f8b4c9
danielsilvayanez <daniel.silva@xing.com>
2020-07-29 09:32:01 +0200

    add login check to dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import TaskList from '../components/TaskList'
+import styled from 'styled-components'
+import Login from '../components/auth/Login'
 
-export default function Dashboard({ tasks }) {
+export default function Dashboard({ tasks, user }) {
+  console.log(user)
   return (
-    <>
-      <TaskList tasks={tasks} />
-    </>
+    <section>
+      {user ? (
+        <>
+          <TaskList tasks={tasks} />
+          <StyledH1>eingeloggt!</StyledH1>
+        </>
+      ) : (
+        <>
+          <Login />
+        </>
+      )}
+    </section>
   )
 }
+
+const StyledH1 = styled.h1`
+  color: black;
+`
